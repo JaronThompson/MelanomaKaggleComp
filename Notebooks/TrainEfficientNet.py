edff505c2c33490ec8e771e7a3e0482f1f9d1660
JaronThompson <jaron.c.thompson@gmail.com>
2020-06-19 22:04:53 -0600

    new submission with larger batch size

--- Notebooks/TrainEfficientNet.py
+++ Notebooks/TrainEfficientNet.py
@@ -305,7 +305,7 @@ valid_loader = torch.utils.data.DataLoader(dataset=valid_dataset)
 
 # save losses from training
 num_epochs = 50
-batchsize  = 64
+batchsize  = 60
 
 train_roc = []
 val_roc   = []
